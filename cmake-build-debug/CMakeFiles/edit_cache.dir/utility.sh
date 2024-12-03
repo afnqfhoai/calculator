9628a51b9f64a96eba94bc7aff627eca61c8675e
FengMing <fengming@bzgzs.cn>
2024-12-03 16:48:46 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/d/Learning/calculator/cmake-build-debug
+/cygdrive/c/Users/26775/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    printf("This is Sun Yat-Sen University's F**king Group Work\n");
+    return 0;
+}
